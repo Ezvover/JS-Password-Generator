8601229dfec0c026fb2d3d54220e24bccba4dc07
Ezvover <vovasik273@gmail.com>
2023-09-10 00:37:47 +0300

    password generating with options

--- app.js
+++ app.js
@@ -5,14 +5,36 @@ var numberOption = document.getElementById('numberOption');
 var digitsAmount = document.getElementById('digitsAmount');
 var generateButton = document.getElementById('generateButton');
 var outputLabel = document.getElementById('outputLabel');
-function GeneratePassword() {
-    var symbolsArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    var answer = "";
-    while (answer.length <= parseInt(digitsAmount.value)) {
-        answer += symbolsArr[Math.floor(Math.random() * symbolsArr.length)];
+function GenereteArray() {
+    if (lettersOption.checked || numberOption.checked) {
+        var symbolsArr = [];
+        if (lettersOption.checked) {
+            if (lowerCaseOption.checked) {
+                symbolsArr.push('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+            }
+            if (upperCaseOption.checked) {
+                symbolsArr.push('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+            }
+        }
+        if (numberOption.checked) {
+            symbolsArr.push('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+        }
+        return symbolsArr;
     }
-    console.log(answer);
-    return answer;
+}
+function GeneratePassword() {
+    try {
+        var symbolsArr = GenereteArray();
+        var answer = "";
+        while (answer.length <= parseInt(digitsAmount.value)) {
+            answer += symbolsArr[Math.floor(Math.random() * symbolsArr.length)];
+        }
+        console.log(answer);
+        return answer;
+    }
+    catch (_a) {
+        alert("Вы не выбрали ни одну из опций");
+    }
 }
 lettersOption.addEventListener('change', function () {
     if (lettersOption.checked) {
--- app.ts
+++ app.ts
@@ -3,25 +3,58 @@ let upperCaseOption = document.getElementById('upperCaseOption') as HTMLInputEle
 let lowerCaseOption = document.getElementById('lowerCaseOption') as HTMLInputElement;
 let numberOption = document.getElementById('numberOption') as HTMLInputElement;
 let digitsAmount = document.getElementById('digitsAmount') as HTMLInputElement;
-
 let generateButton = document.getElementById('generateButton') as HTMLInputElement;
-
 let outputLabel = document.getElementById('outputLabel') as HTMLInputElement;
 
 
+function GenerateArray()
+{
+    if (lettersOption.checked || numberOption.checked)
+    {
+        let symbolsArr : any[] = [];
+        if (lettersOption.checked)
+        {
+            if (lowerCaseOption.checked)
+            {
+                symbolsArr.push('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+            }
+
+            if (upperCaseOption.checked)
+            {
+                symbolsArr.push('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+            }
+        }
+
+        if (numberOption.checked)
+        {
+            symbolsArr.push('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+        }
+
+        return symbolsArr;
+    }
+
+}
 
 function GeneratePassword()
 {
-    let symbolsArr : any[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    let answer : string = "";
-
-    while (answer.length <= parseInt(digitsAmount.value))
+    try
     {
-        answer += symbolsArr[Math.floor(Math.random() * symbolsArr.length)];
+        let symbolsArr = GenerateArray();
+        let answer : string = "";
+
+        while (answer.length <= parseInt(digitsAmount.value))
+        {
+            answer += symbolsArr[Math.floor(Math.random() * symbolsArr.length)];
+        }
+
+        console.log(answer);
+        return answer;
+    }
+    catch
+    {
+        alert("Вы не выбрали ни одну из опций");
     }
 
-    console.log(answer);
-    return answer;
 }
 
 lettersOption.addEventListener('change', function()
